48c704d3b6d62593385910b188ed3b0374209189
arief_el <ariefsamuel21@gmail.com>
2021-05-27 18:33:46 +0700

    add .gitignore, Helloworld and Getting started ps1 file

---
+++ GettingStarted.ps1
@@ -0,0 +1,4 @@
+$today = Read-Host -Prompt "Please enter Date"
+$name = Read-Host -Prompt "Please enter your name"
+Write-Output "Today's date is $today."
+Write-Output "Today is the day $name began their PowerShell programming journey."
